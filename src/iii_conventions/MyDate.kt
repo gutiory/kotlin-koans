dc311d387ca6904b329a023c813d87694d6c4ed8
gutiory <gutiory@gmail.com>
2018-03-06 19:39:36 +0100

    Conventions exercises

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,13 +1,61 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
 
-enum class TimeInterval {
-    DAY,
-    WEEK,
-    YEAR
+    override fun compareTo(other: MyDate): Int {
+        if (year.compareTo(other.year) == 0) {
+            if (month.compareTo(other.month) == 0) {
+                return dayOfMonth.compareTo(other.dayOfMonth)
+            }
+            return month.compareTo(other.month)
+        }
+        return year.compareTo(other.year)
+    }
+
+
+
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+operator fun MyDate.plus(timeInterval: TimeInterval) : MyDate = this.addTimeIntervals(timeInterval, 1)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval) : MyDate =
+        this.addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.n)
+
+
+operator fun TimeInterval.times(n:Int) = RepeatedTimeInterval(this, n)
+
+    enum class TimeInterval {
+        DAY,
+        WEEK,
+        YEAR
+    }
+
+    class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate>{
+        override fun iterator(): Iterator<MyDate> {
+            return object : Iterator<MyDate> {
+
+                var current = start
+
+                override fun hasNext(): Boolean {
+                    val res = current <= endInclusive
+                    return res
+                }
+
+                override fun next(): MyDate {
+                    val res = current
+                    current = current.nextDay()
+                    return res
+                }
+
+            }
+        }
+
+        operator fun contains(d: MyDate) : Boolean {
+            return (start <= d && d <= endInclusive)
+        }
+    }
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval,val n: Int)
