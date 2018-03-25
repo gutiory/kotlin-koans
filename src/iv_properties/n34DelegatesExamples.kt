bf74c5c9b7b6ead6dceb57ac3053fdc904bdeec0
gutiory <gutiory@gmail.com>
2018-03-25 13:54:26 +0200

    Properties exercises

--- src/iv_properties/n34DelegatesExamples.kt
+++ src/iv_properties/n34DelegatesExamples.kt
@@ -1,10 +1,13 @@
 package iv_properties
 
+import org.omg.CORBA.portable.Delegate
 import util.TODO
 import util.doc34
 
 class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by todoTask34()
+    val lazyValue: Int by lazy {
+        initializer()
+    }
 }
 
 fun todoTask34(): Lazy<Int> = TODO(
