22fed10da8e0d081feaeb3cc3e7c4000815cab05
gutiory <gutiory@gmail.com>
2018-04-15 18:20:46 +0200

    Exercises 36 y 37

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -30,10 +30,10 @@ fun todoTask37(): Nothing = TODO(
 
 fun task37(): Map<Int, String> {
     todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
