0c3ab50f26fc0fba4406de190b93e8449567f5ed
gutiory <gutiory@gmail.com>
2018-05-22 14:52:50 +0200

    Exercises 40 and 41

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -19,12 +19,16 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun<T, C: MutableCollection<T>> Iterable<T>.partitionTo(c1: C, c2: C, p: (T) -> Boolean) : Pair<C, C> {
+    c1.addAll(this.filter(p))
+    c2.addAll(this.filterNot(p))
+    return Pair(c1, c2)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
